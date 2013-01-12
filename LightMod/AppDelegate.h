9fd382da2505cab5b432df59eebe6382fef5e49c
Guanshan Liu <guanshan.liu@gmail.com>
2013-01-12 22:06:46 +0800

    Initial Commit

---
+++ LightMod/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LightMod
+//
+//  Created by Guanshan Liu on 12/01/2013.
+//  Copyright (c) 2013 Guanshan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LightMod/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LightMod
+//
+//  Created by Guanshan Liu on 12/01/2013.
+//  Copyright (c) 2013 Guanshan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
